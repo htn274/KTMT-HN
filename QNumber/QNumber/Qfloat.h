4f33c62485e374d06bcfe7286d4f27623a9208cd
lehoanganh298 <lehoanganh29896@gmail.com>
2018-03-12 21:10:02 +0700

    Update

---
+++ QNumber/QNumber/Qfloat.h
@@ -0,0 +1,15 @@
+class Qfloat {
+private:
+	int arr[4];
+public:
+	void ScanQfloat(Qfloat &x);
+	void PrintQfloat(Qfloat x);
+
+	bool * DecToBin(Qfloat x);
+	Qfloat BinToDec(bool *bit);
+
+	Qfloat operator + (Qfloat a);
+	Qfloat operator - (Qfloat a);
+	Qfloat operator * (Qfloat a);
+	Qfloat operator / (Qfloat a);
+};
