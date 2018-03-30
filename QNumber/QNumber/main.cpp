4a9d262ab146286bc662f5adc24635ac5586d767
htn274 <thiennust274@gmail.com>
2018-03-30 17:15:03 +0700

    update main

--- QNumber/QNumber/main.cpp
+++ QNumber/QNumber/main.cpp
@@ -1,22 +1,23 @@
 #pragma once
-#include "CaculateQInt.h"
-#include "Qfloat.h"
+#include "Calculator.h"
 #include <iostream>
-#include "Qfloat.h"
+#include <conio.h>
 using namespace std;
 
 int main() {
-
-
-	Qfloat x,z,a,b,p;
+	/*Qfloat x,z,a,b,p;
 	x.scanDecString("12.2354");
 	for (int i = 0; i < BIT_LENGTH; i++)
 		z.setBitQNum(i, x.getBitQNum(BIT_LENGTH - 1 - i));
+	Qfloat a, b,c;
+	a.ScanQfloat("1.3", 10);
+	b.ScanQfloat("14.5", 10);
+	string ad = a.ToDec();
+	cout << ad << endl;
+	string bd = b.ToDec();
+	cout << bd << endl;
 	
-	cout << z.getExpValue() << endl;
-	z.printTest();
-	string dec = z.ToDec();
-	cout << dec << endl;
+	c = a*b;
 	
 	a.scanDecString("32.1");
 	for (int i = 0; i < BIT_LENGTH; i++)
@@ -29,10 +30,21 @@ int main() {
 	p.printTest();
 	string decp = p.ToDec();
 	cout << decp << endl;
-
-	Qfloat x;
-	x.ScanQfloat("-23.40625", 10);
-	x.PrintQfloat(2);
-
+	*/
+	//Test cho QInt
+	mainCaculate("INPUT.TXT", "OUTPUT.TXT", 0);
+	//Test cho QFloat
+	mainCaculate("","", 1);
+=======
+	string cd = c.ToDec();
+	cout << cd << endl;
+>>>>>>> a26607844b121aa94bb651b165f6e8b52ec18476
+=======
+	
+	c = a*b;
+	
+	string cd = c.ToDec();
+	cout << cd << endl;
+>>>>>>> a26607844b121aa94bb651b165f6e8b52ec18476
 	return 0;
 }
