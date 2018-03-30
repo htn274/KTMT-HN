c70f0aed47bda71d393a2dcf8f2cd92cc72e5bf4
Phạm Khoa Nam <pknam25@gmail.com>
2018-03-30 15:19:20 +0700

    Sửa lại phần scanDec của QFloat với số nhỏ hơn 1

--- QNumber/QNumber/main.cpp
+++ QNumber/QNumber/main.cpp
@@ -1,4 +1,6 @@
+#pragma once
 #include "CaculateQInt.h"
+#include "Qfloat.h"
 #include <iostream>
 
 using namespace std;
@@ -6,30 +8,8 @@ using namespace std;
 
 int main() {
 
-	/*Qfloat x, y, z;
-	x.ScanQfloat("100000000000000010101100", 2);
-	y.ScanQfloat("000000000000000011101011", 2);
-	vector<bool> signi = x.getSignificant();
-	for (int i = 0; i < signi.size(); i++)
-		cout << signi[i];
-	cout << endl;
-	QInt signi_Qint = x.convertToQInt(signi, x.getSign());
-	signi_Qint.printTest();
-	
-	bool sign = x.getSign();
-	vector<bool> signi2 = signi_Qint.toSignedNumber(sign);
-	//cout << sign;
-	for (int i = 0; i < signi2.size(); i++)
-		cout << signi2[i];
-	cout << endl;*/
-
- //Test chương trình QInt
-	mainCaculate("INPUT.TXT", "OUTPUT.TXT");
-// Test chương trình Qfloat
 	Qfloat x;
-	string a = "-23.40625";
-	x.scanDecString(a);
-	x.printTest();
-
+	x.ScanQfloat("0.8", 10);
+	x.PrintQfloat(2);
 	return 0;
 }
